2dcbdf8c5511c080401f7032c001d87b1e24bfc6
Italian-Riky <64906631+Italian-Riky@users.noreply.github.com>
2020-09-05 12:06:56 +0200

    Tradotto embedder.py
    Tradotto embedder.py Dall’inglese all’italiano.

--- embedder/embedder.py
+++ embedder/embedder.py
@@ -1,8 +1,8 @@
 """
-Embedder plugin for Modmail.
+Embed plugin per Modmail.
 
-Written by Papiersnipper.
-All rights reserved.
+Scritto da Papiersnipper(tradotto da Italian-Riky).
+Tutti i diritti riservati.
 """
 
 import re
@@ -18,8 +18,8 @@ from core.models import PermissionLevel
 
 
 class Embedder(Cog):
-    """Easily make embeds for a nicer look.
-    More info: [click here](https://github.com/papiersnipper/modmail-plugins/tree/master/embedder)
+    """Crea embed facilmente per dare un tocco di classe in più al tuo messaggio! (plugin tradotto da [Italian Riky](https://github.com/Italian-Riky))
+    Piu informazioni: [Clicca qua.](https://github.com/papiersnipper/modmail-plugins/tree/master/embedder)
     """
 
     def __init__(self, bot: Bot) -> None:
@@ -29,20 +29,20 @@ class Embedder(Cog):
     @group(name="embedder", aliases=["embed"], invoke_without_command=True)
     @has_permissions(PermissionLevel.MODERATOR)
     async def embedder(self, ctx: Context) -> None:
-        """Easily make embeds for a nicer presence."""
+        """Crea embed facilmente per dare un tocco di classe in più al tuo messaggio!"""
         await ctx.send_help(ctx.command)
 
     @embedder.command(name="color", aliases=["colour"])
     @has_permissions(PermissionLevel.MODERATOR)
     async def color(self, ctx: Context, colorcode: str) -> None:
-        """Save an embed colorcode for future embeds."""
+        """Salva un colore per gli embed futuri."""
         is_valid = re.search(r"^#(?:[0-9a-fA-F]{3}){1,2}$", colorcode)
 
         if not is_valid:
             embed = Embed(
-                title="Embedder",
-                url="https://github.com/papiersnipper/modmail-plugins/blob/master/embedder",
-                description="Please enter a **valid** [hex code](https://htmlcolorcodes.com/color-picker)",
+                title="Embed",
+                url="https://github.com/Italian-Riky/modmail-plugins-1/blob/master/embedder",
+                description="Perfavore metti un **valido** [Codice hex](https://htmlcolorcodes.com/color-picker)",
                 color=self.bot.main_color,
             )
 
@@ -55,9 +55,9 @@ class Embedder(Cog):
         )
 
         embed = Embed(
-            title="Embedder",
-            url="https://github.com/papiersnipper/modmail-plugins/blob/master/embedder",
-            description="This color will be used for every future embed.",
+            title="Embed",
+            url="https://github.com/Italian-Riky/modmail-plugins-1/blob/master/embedder",
+            description="Questo colore verrà usato per tutti i futuri embed!",
             color=Color(int(colorcode, 0)),
         )
 
@@ -94,9 +94,9 @@ class Embedder(Cog):
             await ctx.message.delete()
 
             embed = Embed(
-                title="Embedder",
-                url="https://github.com/papiersnipper/modmail-plugins/blob/master/embedder",
-                description="You can only use one image per embed.",
+                title="Embed",
+                url="https://github.com/Italian-Riky/modmail-plugins-1/blob/master/embedder",
+                description="Puoi solo usare un'immagine per embed.",
                 color=self.bot.main_color,
             )
 
